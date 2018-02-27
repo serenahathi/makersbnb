fc7b6e66adf0806187e2e360a8c7b927d989b257
Lewis Watson <lewwwiswatson@gmail.com>
2018-02-27 11:24:55 +0000

    Connect app.js to listen to 8080, and add html form ' '

--- app.js
+++ app.js
@@ -7,10 +7,27 @@ const app = express();
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(`${__dirname}/views`));
 
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+// app.get('*', (req, res) => res.status(200).send({
+//   message: 'Welcome to the beginning of nothingness.',
+// }));
+
+app.get('/', (req, res) => {
+  res.render('./views/properties.html');
+});
+
+app.get('/properties/new', (req, res) => {
+  res.render('./views/form.html');
+});
+
+app.post('/properties', (req, res) => {
+  res.send(req.params);
+  // console.log(req.params);
+  res.redirect('/');
+});
 
 module.exports = app;
+
+app.listen(8080);
