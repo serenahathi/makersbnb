dcabeb434df016438f63f88591b386bebcb7e018
Laura Chan <laurachan205@gmail.com>
2018-02-27 10:29:28 +0000

    Inititalize Sequelize

---
+++ migrations/20180227102058-create-property.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Properties', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      price: {
+        type: Sequelize.FLOAT
+      },
+      availablefrom: {
+        type: Sequelize.DATEONLY
+      },
+      availableuntil: {
+        type: Sequelize.DATEONLY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Properties');
+  }
+};
